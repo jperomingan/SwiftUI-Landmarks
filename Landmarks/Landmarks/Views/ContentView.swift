21d81a7311909d6ab64880ff3c652c4407e5cb34
jperomingan@startechup.com <jperomingan@startechup.com>
2021-06-01 18:26:14 +0800

    Handling User Input

--- Landmarks/Landmarks/Views/ContentView.swift
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -9,37 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            VStack {
-                MapView()
-                    .frame(height: 300)
-                CircleImage()
-                    .offset(y: -130)
-                    .padding(.bottom, -130)
-                VStack {
-                    Text("Turtle Rock")
-                        .font(.title)
-                }
-                HStack {
-                    Text("Jennelyn Tree National Park")
-                        .font(.subheadline)
-                    Spacer()
-                    Text("California")
-                        .font(.subheadline)
-                }
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-                
-                Divider()
-                
-                Text("About Turtle Rock")
-                    .font(.title2)
-                Text("Descriptive text goes here.")
-            }
-            .padding()
-            
-            Spacer()
-        }
+        LandmarkList()
     }
 }
 
