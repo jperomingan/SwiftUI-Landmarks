951b1260597c0ef8850b8b84052618199eafe37b
jperomingan@startechup.com <jperomingan@startechup.com>
2021-06-01 13:47:33 +0800

    Combine Views Using Stacks

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Jennelyn Urot Peromingan on 6/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Turtle Rock")
+            .font(.title)
+            
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
