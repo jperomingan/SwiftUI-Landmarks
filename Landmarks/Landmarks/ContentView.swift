849155e96a43e9aac5d81132f95b815204717c35
jperomingan@startechup.com <jperomingan@startechup.com>
2021-06-01 15:01:10 +0800

    Compose the Detail View

--- Landmarks/Landmarks/ContentView.swift
+++ Landmarks/Landmarks/ContentView.swift
@@ -11,6 +11,11 @@ struct ContentView: View {
     var body: some View {
         HStack {
             VStack {
+                MapView()
+                    .frame(height: 300)
+                CircleImage()
+                    .offset(y: -130)
+                    .padding(.bottom, -130)
                 VStack {
                     Text("Turtle Rock")
                         .font(.title)
@@ -22,8 +27,18 @@ struct ContentView: View {
                     Text("California")
                         .font(.subheadline)
                 }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
             }
             .padding()
+            
+            Spacer()
         }
     }
 }
